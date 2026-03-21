5491d9d689ac4c0d630d3884a1401696d0369c83
coldelectrons <frithomas@gmail.com>
2026-03-21 11:03:04 -0400

    added neovide keybinds

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -55,7 +55,22 @@
     scrolloff = 4;
 
     laststatus = 3;
+
   };
+
+  extraConfigLua = ''
+    if vim.g.neovide then
+      vim.keymap.set('n', '<C-s>', ':w<CR>') -- Save
+      vim.keymap.set('v', '<C-c>', '"+y') -- Copy
+      vim.keymap.set('n', '<C-v>', '"+P') -- Paste normal mode
+      vim.keymap.set('v', '<C-v>', '"+P') -- Paste visual mode
+      vim.keymap.set('c', '<C-v>', '<C-R>+') -- Paste command mode
+      vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
+      vim.keymap.set({ "n", "v" }, "<C-= >", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+      vim.keymap.set({ "n", "v" }, "<C-- >", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+      vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+    end
+  '';
 
   keymaps = [
     {
